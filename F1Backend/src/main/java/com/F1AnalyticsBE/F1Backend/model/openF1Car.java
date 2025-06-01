6f368652a3ce34f7b911f26c86381bd8c750316f
Lehlanya25 <sebabatso.moleele@mpilo.africa>
2025-06-01 13:59:30 +0200

    Create openF1Car.java

---
+++ F1Backend/src/main/java/com/F1AnalyticsBE/F1Backend/model/openF1Car.java
@@ -0,0 +1,12 @@
+package com.F1AnalyticsBE.F1Backend.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class openF1Car {
+}
